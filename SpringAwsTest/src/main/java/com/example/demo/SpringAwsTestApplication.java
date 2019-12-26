5dd900535e2438effb1267cb0df6ba916f374604
gunbin <gunbin91@gmail.com>
2019-12-26 14:22:55 +0900

    외부 톰캣 테스트 성공

--- SpringAwsTest/src/main/java/com/example/demo/SpringAwsTestApplication.java
+++ SpringAwsTest/src/main/java/com/example/demo/SpringAwsTestApplication.java
@@ -1,12 +1,25 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
-public class SpringAwsTestApplication {
-
+public class SpringAwsTestApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringAwsTestApplication.class);
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAwsTestApplication.class, args);
 		System.out.println("세팅완료");
